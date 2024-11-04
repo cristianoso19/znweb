ea0317976463f73234c4f51a1cdb5880a4787b78
cristianFedora <cristianoso19@gmail.com>
2024-11-04 00:24:30 -0500

    agregar DaisyUI como dependencia y actualizar la configuración de Tailwind CSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5a3bf9",
+
+          secondary: "#bd1af0",
+
+          accent: "#d8078a",
+
+          neutral: "#040404",
+
+          "base-100": "#f6fefa",
+
+          info: "#05a9ff",
+
+          success: "#51c000",
+
+          warning: "#e16200",
+
+          error: "#c10035",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
